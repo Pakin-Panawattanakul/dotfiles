d8f9a3ae3b8d47c7d3498ac625424285b27ea931
Pakin Panawattanakul <pakin.pan@proton.me>
2026-04-13 21:33:11 +0700

    fix void-install scripts

--- files/Scripts/install/void-install.sh
+++ files/Scripts/install/void-install.sh
@@ -20,7 +20,7 @@ sudo xbps-install -y void-repo-nonfree void-repo-multilib void-repo-multilib-non
 sudo xbps-install -Syu
 
 # essential
-sudo xbps-install -y intel-ucode base-devel dbus elogind polkitd man-db
+sudo xbps-install -y intel-ucode base-devel dbus elogind polkit man-db
 sudo rm /var/service/wpa_supplicant
 sudo ln -s /etc/sv/dbus  /etc/sv/polkitd /var/service
 # /etc/sv/elogind not enable this and enable greetd and tuigreet at the end
@@ -28,6 +28,7 @@ sudo ln -s /etc/sv/dbus  /etc/sv/polkitd /var/service
 # log
 sudo xbps-install -y socklog-void
 sudo ln -s /etc/sv/socklog-unix /etc/sv/nanoklogd /var/service
+
 sudo xbps-install -y xdg-user-dirs
 xdg-user-dirs-update
 
@@ -42,10 +43,14 @@ sudo ln -s /etc/sv/bluetoothd /var/service
 
 # sound
 sudo xbps-install -y pipewire wireplumber wiremix libspa-bluetooth
-sudo mkdir -p /etc/pipewire/pipewire.conf.d
-sudo ln -s /usr/share/examples/wireplumber/10-wireplumber.conf /etc/pipewire/pipewire.conf.d/
-sudo mkdir -p /etc/pipewire/pipewire.conf.d
-sudo ln -s /usr/share/examples/pipewire/20-pipewire-pulse.conf /etc/pipewire/pipewire.conf.d/
+: "${XDG_CONFIG_HOME:=${HOME}/.config}"
+mkdir -p "${XDG_CONFIG_HOME}/pipewire/pipewire.conf.d"
+ln -s /usr/share/examples/wireplumber/10-wireplumber.conf "${XDG_CONFIG_HOME}/pipewire/pipewire.conf.d/"
+ln -s /usr/share/examples/pipewire/20-pipewire-pulse.conf "${XDG_CONFIG_HOME}/pipewire/pipewire.conf.d/"
+
+# rtkit
+sudo xbps-install -y rtkit
+sudo ln -s /etc/sv/rtkit /var/service
 
 # ntp
 sudo xbps-install -y openntpd
@@ -56,11 +61,8 @@ sudo xbps-install -y tlp upower brightnessctl
 sudo tlp start
 sudo ln -s /etc/sv/tlp /var/service
 
-cd "$HOME/dotfiles"
-git submodule update --init --recursive
-
 # dwl dependecies
-sudo xbps-install -y libinput libinput-devel wayland wayland-devel wlroots0.19 wlroots0.19-devel libxkbcommon mibxkbcommon-devel wayland-protocols pkg-config xorg-server-xwayland
+sudo xbps-install -y libinput libinput-devel wayland wayland-devel wlroots0.19 wlroots0.19-devel libxkbcommon libxkbcommon-devel wayland-protocols pkg-config xorg-server-xwayland
 # someblock
 sudo xbps-install -y cairo-devel  pango-devel meson ninja
 
@@ -71,37 +73,39 @@ sudo xbps-install -y xdg-desktop-portal xdg-desktop-portal-wlr xdg-desktop-porta
 sudo xbps-install -y wl-clipboard grim slurp wbg waylock wlr-randr wdisplays wl-mirror wev 
 
 # desktop utils
-sudo xbps-install -y wmenu libnotify mako imv zathura zathura-pdf-mupdf aria2c
+sudo xbps-install -y wmenu libnotify mako imv zathura zathura-pdf-mupdf aria2
 
 # themes and fonts
 sudo xbps-install -y papirus-icon-theme noto-fonts-ttf nerd-fonts-ttf nwg-look qt6-wayland
 
 # terminal
-sudo xbps-install -y foot foot-terminfo zsh eza zoxide starship \
-  fzf fd ncdu ripgrep tree
+sudo xbps-install -y foot foot-terminfo bat zsh eza zoxide starship \
+  fzf fd ncdu ripgrep tree fastfetch
 
 # yazi 
-sudo xbps-install -y poppler resvg ImageMagick
+sudo xbps-install -y yazi poppler resvg ImageMagick ffmpeg
 
 # dev 
-sudo xbps-install -y rust cargo python3-pip gdb git-lfs lazygit
+sudo xbps-install -y rust cargo python3-pip gdb git git-lfs lazygit
 
 # neovim
 sudo xbps-install -y neovim tree-sitter tree-sitter-devel nodejs luarocks
 
-# utils
-sudo xbps-install -y btop jq glxinfo
+# utils / software
+sudo xbps-install -y curl wget btop stow jq glxinfo tldr qmk thunderbird libreoffice libreoffice-i18n-th
 
 # Media 
-sudo xbps-install -y mpd mpc rmpc cava mpv yt-dlp
+sudo xbps-install -y qutebrowser mpd mpc rmpc cava mpv yt-dlp
 
 # flatpak
-sudo xbps-install flatpak
-flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
-flatpak install --assumeyes dev.vencord.Vesktop  me.proton.Pass  com.spotify.Client io.gitlab.librewolf-community 
+sudo xbps-install -y flatpak
+flatpak remote-add --user --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+flatpak install --user --assumeyes dev.vencord.Vesktop  me.proton.Pass  com.spotify.Client io.gitlab.librewolf-community  net.davidotek.pupgui2
+# balatro mod mamager
+curl -sL https://raw.githubusercontent.com/skyline69/balatro-mod-manager/main/scripts/linux-install.sh | bash -s -- --clone
 
 # game 
-sudo xbps-install -y steam lib-drm32bit libgcc-32bit libstdc++-32bit xdg-utils vulkan-loader-32bit
+sudo xbps-install -y steam libdrm-32bit libgcc-32bit libstdc++-32bit gamemode
 
 #https://bugraeren.com/blog/vivado_on_void_linux/
 # --- vivado ---
@@ -119,6 +123,11 @@ sudo xbps-install -y gtk+3 gtk+3-32bit # gtk3
 sudo cp /lib/libtinfo.so.6 /lib/libtinfo.so.5
 sudo cp /lib/libncurses.so.6 /lib/libncurses.so.5
 
+# ZSA keymapp dependency & proton pass
+sudo cp "$install_dir/50-zsa.rules" "/etc/udev/rules.d/50-zsa.rules"
+sudo groupadd plugdev
+sudo usermod -aG plugdev $USER
+
 # nvidia
 choice=""
 while [ "$choice" != "y" ] && [ "$choice" != "n" ]; do
@@ -127,18 +136,12 @@ while [ "$choice" != "y" ] && [ "$choice" != "n" ]; do
 done
 
 install_nvidia(){
-  sudo xbps-install mesa-dri nvidia  nvidia-lib32-bit
+  sudo xbps-install mesa-dri nvidia  nvidia-libs-32bit
   sudo sed -i '/^GRUB_CMDLINE_LINUX_DEFAULT/c\GRUB_CMDLINE_LINUX_DEFAULT="loglevel=4 nvidia_drm.modeset=1 nvidia_drm.fbdev=1"' /etc/default/grub
 }
-if [ $choice -eq 'y' ]; do
-  #install_nvidia
+if [ $choice = 'y' ]; do
+  install_nvidia
 done
-
-# ZSA keymapp dependency & proton pass
-sudo cp "$install_dir/50-zsa.rules" "/etc/udev/rules.d/50-zsa.rules"
-sudo groupadd plugdev
-sudo usermod -aG plugdev $USER
-
 
 sudo xbps-install greetd tuigreet
 sudo ln -s /etc/sv/greetd /var/service
