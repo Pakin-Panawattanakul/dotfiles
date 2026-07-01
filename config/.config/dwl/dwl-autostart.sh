dcafcac0ac203b1ea170fb25a45de751cf24abbb
Pakin Panawattanakul <pakin.pan@proton.me>
2026-07-01 22:04:21 +0700

    move dwl personal config that often change to .dotfiles instead of dwl

---
+++ config/.config/dwl/dwl-autostart.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+$HOME/Scripts/low_battery.sh &
+wbg "$HOME/Pictures/wallpapers/astronaut.png" &
+brightnessctl set 50%
+
+dbus-update-activation-environment DISPLAY WAYLAND_DISPLAY XDG_CURRENT_DESKTOP=wlroots
+
+pkill -x pipewire
+pkill -x mpd 
+pkill -x mako
+sleep 0.5
+pipewire &
+mpd &
+mako &
