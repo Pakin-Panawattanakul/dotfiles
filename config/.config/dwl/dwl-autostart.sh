d2517986c10aa429c932d9b63c1c9a3612542d3a
Pakin Panawattanakul <pakin.pan@proton.me>
2026-07-07 01:57:26 +0700

    change dwl wallpaper and foot bg

--- config/.config/dwl/dwl-autostart.sh
+++ config/.config/dwl/dwl-autostart.sh
@@ -1,13 +1,11 @@
 #!/bin/sh
 
 $HOME/Scripts/low_battery.sh &
-wbg "$HOME/Pictures/wallpapers/astronaut.png" &
+wbg "$HOME/Pictures/wallpapers/void_071.png" &
 brightnessctl set 50%
 
 dbus-update-activation-environment DISPLAY WAYLAND_DISPLAY XDG_CURRENT_DESKTOP=wlroots
 
-#wlr-randr --output DP-3 --mode 1920x1080@165
-#wlr-randr --output eDP-1 --mode 1920x1080@165
 pkill -x pipewire
 pkill -x mpd 
 pkill -x mako
