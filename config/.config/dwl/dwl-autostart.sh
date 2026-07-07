6229d6a49ddf3a887d198a50b3a021663b2bb463
Pakin Panawattanakul <pakin.pan@proton.me>
2026-07-07 12:02:47 +0700

    remove comment in foot.ini

--- config/.config/dwl/dwl-autostart.sh
+++ config/.config/dwl/dwl-autostart.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 $HOME/Scripts/low_battery.sh &
-wbg "$HOME/Pictures/wallpapers/void_071.png" &
+wbg "$HOME/Pictures/wallpapers/void_019.png" &
 brightnessctl set 50%
 
 dbus-update-activation-environment DISPLAY WAYLAND_DISPLAY XDG_CURRENT_DESKTOP=wlroots
