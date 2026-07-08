0811af4dfef33dd052ffb42286cab211e52dbf88
Pakin Panawattanakul <pakin.pan@proton.me>
2026-07-08 22:28:03 +0700

    back to telescope

--- config/.config/nvim/lua/config/lsp.lua
+++ config/.config/nvim/lua/config/lsp.lua
@@ -1,16 +1,16 @@
 -- luacheck: globals vim
 -- Add Blink.cmp capabilities
 vim.lsp.config("slang-server", {
-  cmd = { "slang-server" },
-  root_markers = { ".git", ".slang" },
-  filetypes = {
-    "systemverilog",
-    "verilog",
-  },
+	cmd = { "slang-server" },
+	root_markers = { ".git", ".slang" },
+	filetypes = {
+		"systemverilog",
+		"verilog",
+	},
 })
 
 vim.lsp.enable("slang-server")
-vim.cmd('cnoreabbrev LspInfo checkhealth vim.lsp')
+vim.cmd("cnoreabbrev LspInfo checkhealth vim.lsp")
 
 local capabilities = require("blink.cmp").get_lsp_capabilities()
 vim.lsp.config("*", {
@@ -22,34 +22,15 @@ local map = vim.keymap.set
 -- Only create this keymap when lsp attach to buffer
 vim.api.nvim_create_autocmd("LspAttach", {
 	callback = function(ev)
+		local builtin = require("telescope.builtin")
 		map("n", "<leader>rn", vim.lsp.buf.rename, { buffer = ev.buf, desc = "LSP : Rename" })
 		map({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, { buffer = ev.buf, desc = "LSP : Code Action" })
-		map(
-			"n",
-			"<leader>gw",
-			":Pick lsp scope='workspace_symbol_live'<cr>",
-			{ buffer = ev.buf, desc = "LSP : workspace symbols" }
-		)
-		map(
-			"n",
-			"<leader>gs",
-			":Pick lsp scope='document_symbol'<cr>",
-			{ buffer = ev.buf, desc = "LSP : Document Symbols" }
-		)
-		map(
-			"n",
-			"gR",
-			":Pick lsp scope='references'<cr>",
-			{ nowait = true, buffer = ev.buf, desc = "LSP : [G]o to [R]efferences" }
-		)
-		map("n", "gd", ":Pick lsp scope='definition'<cr>", { buffer = ev.buf, desc = "LSP : [D]efinitions" })
-		map("n", "gD", ":Pick lsp scope='declaration'<cr>", { buffer = ev.buf, desc = "LSP : [G]o to [D]eclaration" })
-		map(
-			"n",
-			"gi",
-			"Pick lsp scope='implementatioin'<cr>",
-			{ buffer = ev.buf, desc = "LSP : [G]oto [I]mplemetations" }
-		)
+		map("n", "<leader>gw", builtin.lsp_workspace_symbols, { buffer = ev.buf, desc = "LSP : workspace symbols" })
+		map("n", "<leader>gs", builtin.lsp_document_symbols, { buffer = ev.buf, desc = "LSP : Document Symbols" })
+		map("n", "gr", builtin.lsp_references, { nowait = true, buffer = ev.buf, desc = "LSP : [G]o to [R]eferences" })
+		map("n", "gd", builtin.lsp_definitions, { buffer = ev.buf, desc = "LSP : [D]efinitions" })
+		map("n", "gD", vim.lsp.buf.declaration, { buffer = ev.buf, desc = "LSP : [G]o to [D]eclaration" })
+		map("n", "gi", builtin.lsp_implementations, { buffer = ev.buf, desc = "LSP : [G]oto [I]mplementations" })
 		map("n", "K", vim.lsp.buf.hover, { buffer = ev.buf, desc = "LSP : Displays hover information" })
 		map("n", "<leader>rs", ":LspRestart<CR>", { buffer = ev.buf, desc = "LSP : restart LSP" })
 	end,
