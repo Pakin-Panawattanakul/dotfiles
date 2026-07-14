df4da0ce89ae1ad7448f542cbee8e0322a9b0d24
Pakin Panawattanakul <p.panawattanakul@gmail.com>
2026-07-15 02:15:52 +0700

    fixed bash syntax issues

--- config/.config/dwl/dwl-autostart.sh
+++ config/.config/dwl/dwl-autostart.sh
@@ -7,7 +7,7 @@ pkill -x mako
 mako &
 if [ "$HOST" = "void-linux-PC" ]; then
   wlr-randr --output DP-3 --mode 1920x1080@165Hz
-else if [ "$HOST" = "void-linux" ]; then
+elif [ "$HOST" = "void-linux" ]; then
   wlr-randr --output eDP-1 --mode 1920x1080@165Hz
 fi
 
