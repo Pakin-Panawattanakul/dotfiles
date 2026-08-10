766eb17c934b7fc4bbc5ab2836803721d0b044ce
Pakin Panawattanakul <p.panawattanakul@gmail.com>
2026-08-11 00:39:48 +0700

    nix + mango : try out fcitx5

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -26,6 +26,14 @@
   # Set your time zone.
   time.timeZone = "Asia/Bangkok";
   i18n.defaultLocale = "en_US.UTF-8";
+  i18n.inputMethod = {
+    type = "fcitx5";
+    enable = true;
+    fcitx5.addons = with pkgs; [
+      fcitx5-mozc
+      fcitx5-gtk
+    ];
+  };
 
   # Configure network connections interactively with nmcli or nmtui.
   networking.networkmanager.enable = true;
--- nixos/home.nix
+++ nixos/home.nix
@@ -1,4 +1,9 @@
-{ config, pkgs, pkgs-unstable, ... }:
+{
+  config,
+  pkgs,
+  pkgs-unstable,
+  ...
+}:
 {
   home.username = "pakin";
   home.homeDirectory = "/home/pakin";
