28834b1ba8d512c59ca4c5ee820d323d679998dd
Pakin Panawattanakul <p.panawattanakul@gmail.com>
2026-08-13 01:25:36 +0700

    nix : tlp cpu treshold

--- nixos/modules/battery.nix
+++ nixos/modules/battery.nix
@@ -10,10 +10,10 @@
       CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
       CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
 
-      # CPU_MIN_PERF_ON_AC = 0;
-      # CPU_MAX_PERF_ON_AC = 100;
-      # CPU_MIN_PERF_ON_BAT = 0;
-      # CPU_MAX_PERF_ON_BAT = 75;
+      CPU_MIN_PERF_ON_AC = 0;
+      CPU_MAX_PERF_ON_AC = 100;
+      CPU_MIN_PERF_ON_BAT = 0;
+      CPU_MAX_PERF_ON_BAT = 80;
 
       # Optional helps save long term battery health
       START_CHARGE_THRESH_BAT0 = 40;
