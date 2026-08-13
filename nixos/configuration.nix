9d243a33c56f99042bd08405832784bcae5224a7
Pakin Panawattanakul <p.panawattanakul@gmail.com>
2026-08-13 22:26:47 +0700

    nix: systemd boot limit to 3

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -19,7 +19,7 @@
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
-  boot.loader.systemd-boot.configurationLimit = 2;
+  boot.loader.systemd-boot.configurationLimit = 3;
 
   # Use latest kernel.
   boot.kernelPackages = pkgs.linuxPackages_latest;
