de3848d8738dc9c92ad545a8a49e94f4daac03ec
Pakin Panawattanakul <p.panawattanakul@gmail.com>
2026-08-15 13:06:14 +0700

    nix NV15 : enable virtualbox

--- flake.nix
+++ flake.nix
@@ -74,7 +74,8 @@
             ./hosts/hardware-configuration-NV15.nix
             ./configuration.nix
             ./modules/battery.nix
-	    ./modules/nvidia.nix
+	          ./modules/nvidia.nix
+            ./modules/virtualbox.nix
             #./packages/nixos-T480.nix
             { networking.hostName = "nixos-NV15"; }
             home-manager.nixosModules.home-manager
