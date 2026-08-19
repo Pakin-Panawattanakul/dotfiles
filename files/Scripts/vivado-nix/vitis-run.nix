b5984936cbba8e0f567f0bd3bf4cf12bd060c053
Pakin Panawattanakul <p.panawattanakul@gmail.com>
2026-08-19 21:28:14 +0700

    vivado: move and add all viado/vitis programs

---
+++ files/Scripts/vivado-nix/vitis-run.nix
@@ -0,0 +1,49 @@
+{ pkgs ? import <nixpkgs> { } }:
+(pkgs.buildFHSEnv {
+  name = "vivado-env";
+  targetPkgs = pkgs: (
+    with pkgs; [
+      ncurses5
+      ncurses
+      libxcrypt-legacy
+      libpng
+      libusb1
+      systemd
+      pixman
+      zlib
+      libuuid
+      bash
+      coreutils
+      zlib
+      stdenv.cc.cc
+      libXext
+      libX11
+      libXrender
+      libXtst
+      libXi
+      libXft
+      libxcb
+      libxcb
+      freetype
+      fontconfig
+      glib
+      gtk2
+      gtk3
+      graphviz
+      gcc
+      unzip
+      nettools
+    ]
+  );
+  profile = ''
+    export LD_LIBRARY_PATH=/usr/lib:/usr/lib64:$LD_LIBRARY_PATH
+  '';
+  runScript = ''
+    env LIBRARY_PATH=/usr/lib \
+      C_INCLUDE_PATH=/usr/include \
+      CPLUS_INCLUDE_PATH=/usr/include \
+      CMAKE_LIBRARY_PATH=/usr/lib \
+      CMAKE_INCLUDE_PATH=/usr/include \
+      /opt/Xilinx/2026.1/Vitis/bin/vitis
+  '';
+}).env
