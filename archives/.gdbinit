df44f30cd4b824e1851da33a34c591388596a5ec
Pakin Panawattanakul <p.panawattanakul@gmail.com>
2026-09-14 17:19:30 +0700

    move archive to separate repo

--- archives/.gdbinit
+++
@@ -1,2 +0,0 @@
-set debuginfod enabled on
-with height 0 -- layout src
--- archives/i3/keyboard.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-setxkbmap -layout us,th -variant colemak_dh, -option 'grp:win_space_toggle,lv3:caps_switch,shift:both_capslock'
-sleep 0.2
-xmodmap -e "keycode 43 = m M Thai_maitho Thai_maitaikhu h H" \
--e "keycode 44 = n N Thai_maiek Thai_maichattawa j J" \
--e "keycode 45 = e E Thai_saraaa Thai_sorusi k K" \
--e "keycode 46 = i I Thai_sosua Thai_sosala l L"
